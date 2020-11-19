3b5480f1ecd1a915f0f43f7167048e85ae04685e
laurakessas <32477876+laurakessas@users.noreply.github.com>
2020-11-19 15:46:21 +0100

    ArticleController + vue

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/article", name="article")
+     */
+    public function index(): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+
+    public function getAllArticles(ArticleRepository $articleRepository)
+    {
+        //   $articles = $articleRepository->findAllArticle();
+    }
+}
