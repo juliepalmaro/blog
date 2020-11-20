cc2d48ace5f646d443b653e0cc605b248c13b227
Romain <iaco2702@gmail.com>
2020-11-20 15:18:54 +0100

    ajout de knp-paginator

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -109,4 +109,6 @@ class PublicController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+
 }
