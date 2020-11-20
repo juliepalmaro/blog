4fefbfaf416f769f835de99cce0ecb0c846826e2
albanpapassian@gmail.com <albanpapassian@gmail.com>
2020-11-20 11:26:52 +0100

    new article form

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ArticleNewType;
 
 class AdminDashboardController extends AbstractController
 {
@@ -23,11 +26,18 @@ class AdminDashboardController extends AbstractController
     /**
      * @Route("/admin/articles/new", name="admin_article_new")
      */
-    public function createArticle(ArticleRepository $articleRepository): Response
+    public function createArticle(Request $request, ArticleRepository $articleRepository): Response
     {
-        $articles = $articleRepository->findAllArticles(0, 10, null, null, null, null, null);
-        dump($articles);
+        $article = new Article();
+        $form = $this->createForm(ArticleNewType::class, $article);
+        $form->handleRequest($request);
 
-        return $this->render('admin_dashboard/index.html.twig', []);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $articleRepository->createOrUpdateArticle($article);
+        }
+
+        return $this->render('admin_dashboard/newArticle.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
