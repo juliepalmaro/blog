72a0003021441582560f4a3e1daa66c04906e3b1
Romain <iaco2702@gmail.com>
2020-11-20 12:54:01 +0100

    rename article html.twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -13,7 +13,7 @@ class ArticleController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('article/index.html.twig', [
+        return $this->render('public/article.html.twig', [
             'controller_name' => 'ArticleController',
         ]);
     }
